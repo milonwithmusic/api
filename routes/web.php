9effd8985b0a6441b25fabc8374222ebee79e60b
Firoz Siddique <milonwithmusic@gmail.com>
2025-09-30 10:18:42 +0600

    Created Model -a

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::apiResource('/Products', 'ProductController'); 
+
+Route::group(['prefix'=>'products'],function(){
+Route::apiResource('/{product}/reviews','ReviewController');
+});
